a80b9281c122f7af83c8b958146093ebfe15cf85
Natalia <alimadeoliveiranatalia@gmail.com>
2022-03-04 19:29:22 -0300

    Implementando try/catch

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,12 +6,13 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { name, email } = request.body;
-    const user = this.createUserUseCase.execute({ name, email });
-    if (!user) {
-      return response.status(400).json({ error: "Not found!" });
+    try {
+      const { name, email } = request.body;
+      const user = this.createUserUseCase.execute({ name, email });
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(400).json(error);
     }
-    return response.status(201).json(user);
   }
 }
 
