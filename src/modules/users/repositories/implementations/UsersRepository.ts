47c64ee3364e9c5fe6f78c1b71cff87396420e6e
Natalia <alimadeoliveiranatalia@gmail.com>
2022-03-03 21:42:07 -0300

    model e repository implementados

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,36 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    Object.assign(user, {
+      name,
+      admin: false,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const findId = this.users.find((user) => user.id === id);
+    return findId;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const findUser = this.users.find((user) => user.email === email);
+    return findUser;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    receivedUser.admin = true;
+    receivedUser.updated_at = new Date();
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
